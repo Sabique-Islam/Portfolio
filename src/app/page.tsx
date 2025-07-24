46f51e754d61ad7ebf5f2d615344f194b49e82f2
sabique <sabiqueislam7@gmail.com>
2025-07-25 01:41:36 +0530

    feat: wrap Skills Section in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Footer from "@/components/Pages/Footer";
 import Hero from "@/components/Pages/Hero";
 import { Navbar } from "@/components/Pages/Navbar";
 import Projects from "@/components/Pages/Projects";
+import Skills from "@/components/Pages/Skills";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <Projects/>
+      <Skills/>
       <Navbar/>
       <Footer/>
     </div>
